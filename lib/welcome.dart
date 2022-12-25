3a445458a8050428c038b66a0e80c70d565e6ece
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-12-25 23:24:50 +0100

    Update

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 
 class Blagajna extends StatelessWidget {
   const Blagajna({super.key});
@@ -35,68 +39,115 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      drawer: const Drawer(),
+      body: FooterView(
+          footer: Footer(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          SizedBox(
+                              height: 45.0,
+                              width: 45.0,
+                              child: Center(
+                                child: Card(
+                                  elevation: 5.0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        25.0), // half of height and width of Image
+                                  ),
+                                  child: IconButton(
+                                    icon: const Icon(
+                                      Icons.audiotrack,
+                                      size: 20.0,
+                                    ),
+                                    color: const Color(0xFF162A49),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              )),
+                          SizedBox(
+                              height: 45.0,
+                              width: 45.0,
+                              child: Center(
+                                child: Card(
+                                  elevation: 5.0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        25.0), // half of height and width of Image
+                                  ),
+                                  child: IconButton(
+                                    icon: const Icon(
+                                      Icons.fingerprint,
+                                      size: 20.0,
+                                    ),
+                                    color: const Color(0xFF162A49),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              )),
+                          SizedBox(
+                              height: 45.0,
+                              width: 45.0,
+                              child: Center(
+                                child: Card(
+                                  elevation: 5.0,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(
+                                        25.0), // half of height and width of Image
+                                  ),
+                                  child: IconButton(
+                                    icon: const Icon(
+                                      Icons.call,
+                                      size: 20.0,
+                                    ),
+                                    color: const Color(0xFF162A49),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              )),
+                        ],
+                      ),
+                    ),
+                    const Text(
+                      'Andrej Bartulin, 2022, Public Domain',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                          fontSize: 12.0,
+                          color: Color(0xFF162A49)),
+                    ),
+                  ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          children: <Widget>[            
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(300, 80)),
+              child: const Text("Login"),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            const Text(" "),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(20.0),
+                  fixedSize: const Size(300, 80)),
+              child: const Text("Sign up"),
+            )
+          ]),
     );
   }
 }
