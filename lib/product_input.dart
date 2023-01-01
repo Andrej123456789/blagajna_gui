42ce61b2dfcf06590205e0f6c3e333e78c4b8346
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-01-01 23:59:54 +0100

    Improved GUI menu (better scaling, Happy New Year)

--- lib/product_input.dart
+++ lib/product_input.dart
@@ -16,118 +16,143 @@ class MenuInput extends State<StatefulWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-        // Setting floatHeaderSlivers to true is required in order to float
-        // the outer slivers over the inner scrollable.
-        floatHeaderSlivers: true,
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              title: const Text('GUI menu'),
-              floating: true,
-              expandedHeight: 50.0,
-              forceElevated: innerBoxIsScrolled,
-            ),
-          ];
-        },
-        body: Stack(
-          children: [
-            Positioned(
-                top: 10,
-                left: 50,
-                child: IconButton(
-                  onPressed: () {
-                    outputBox.addProduct("3850102520381", price: 1.60);
-                    money.addMoney(1.60);
-                  },
-                  icon: Image.asset("assets/products/Čokolada Dorina.jpg"),
-                  iconSize: 250,
-                )),
-            const Positioned(
-                top: 260, left: 105, child: Text("Chocolate Dorina (1.60 €)")),
-            Positioned(
-                top: 10,
-                left: 350,
-                child: IconButton(
-                  onPressed: () {
-                    outputBox.addProduct("9789531976152", price: 17.88);
-                    money.addMoney(17.88);
-                  },
-                  icon: Image.asset(
-                      "assets/products/Knjiga - Domagoj Kusalić - Napredno programiranje i algoritmi u C-u i C++-u.jpg"),
-                  iconSize: 250,
-                )),
-            const Positioned(
-                top: 280,
-                left: 410,
-                child: Text(
-                    "Domagoj Kusalić - \nNapredno programiranje i \nalgoritmi u C-u i C++-u \n              (17.88 €)")),
-            Positioned(
-                top: 10,
-                left: 650,
-                child: IconButton(
-                  onPressed: () {
-                    outputBox.addProduct("9789542991152", price: 21.06);
-                    money.addMoney(21.06);
-                  },
-                  icon: Image.asset(
-                      "assets/products/Knjiga - Željko Kovačević - C++, analiza i primjena.jpg"),
-                  iconSize: 250,
-                )),
-            const Positioned(
-                top: 280,
-                left: 690,
-                child: Text(
-                    "Željko Kovačević - C++, analiza i \nprimjena (21.06 €)")),
-            Positioned(
-                top: 350,
-                left: 50,
-                child: IconButton(
-                  onPressed: () {
-                    outputBox.addProduct("4096542991152", price: 6.64);
-                    money.addMoney(6.64);
-                  },
-                  icon: Image.asset("assets/products/Kuhača drvena.jpg"),
-                  iconSize: 250,
-                )),
-            const Positioned(
-                top: 650, left: 110, child: Text("Kuhača drvena (6.64 €)")),
-            Positioned(
-                top: 350,
-                left: 350,
-                child: IconButton(
-                  onPressed: () {
-                    outputBox.addProduct("0360002914523", price: 13.37);
-                    money.addMoney(13.37);
-                  },
-                  icon: Image.asset(
-                      "assets/products/Lektira - Voda.jpg"),
-                  iconSize: 250,
-                )),
-            const Positioned(
-                top: 650,
-                left: 410,
-                child: Text(
-                    "Lektira - Voda (13.37 €)")),
-            Positioned(
-                top: 350,
-                left: 650,
-                child: IconButton(
-                  onPressed: () {
-                    outputBox.addProduct("9789151976152", price: 3.32);
-                    money.addMoney(3.32);
-                  },
-                  icon: Image.asset(
-                      "assets/products/Orink magenta - tinta za printer.jpg"),
-                  iconSize: 250,
-                )),
-            const Positioned(
-                top: 650,
-                left: 660,
-                child: Text(
-                    "Orink magenta - tinta za printer (3.32 €)")),
-          ],
-        ),
-      ),
+          // Setting floatHeaderSlivers to true is required in order to float
+          // the outer slivers over the inner scrollable.
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: const Text('GUI menu'),
+                floating: true,
+                expandedHeight: 50.0,
+                forceElevated: innerBoxIsScrolled,
+              ),
+            ];
+          },
+          body: Center(
+              child: Wrap(
+            alignment: WrapAlignment.spaceAround,
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              outputBox.addProduct("3850102520381",
+                                  price: 1.60);
+                              money.addMoney(1.60);
+                            },
+                            icon: Image.asset(
+                                "assets/products/Čokolada Dorina.jpg"),
+                            iconSize: 250,
+                          ),
+                          const Text("Chocolate Dorina (1.60 €)")
+                        ],
+                      )),
+                  const SizedBox(width: 25, height: 25),
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              outputBox.addProduct("4096542991152",
+                                  price: 6.64);
+                              money.addMoney(6.64);
+                            },
+                            icon: Image.asset(
+                                "assets/products/Kuhača drvena.jpg"),
+                            iconSize: 250,
+                          ),
+                          const Text("Kuhača drvena (6.64 €)")
+                        ],
+                      )),
+                ],
+              ),
+              const SizedBox(width: 30, height: 30),
+              Column(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              outputBox.addProduct("9789531976152",
+                                  price: 17.88);
+                              money.addMoney(17.88);
+                            },
+                            icon: Image.asset(
+                                "assets/products/Knjiga - Domagoj Kusalić - Napredno programiranje i algoritmi u C-u i C++-u.jpg"),
+                            iconSize: 250,
+                          ),
+                          const Text(
+                              "Domagoj Kusalić - Napredno programiranje i \nalgoritmi u C-u i C++-u (17.88 €)")
+                        ],
+                      )),
+                  const SizedBox(width: 25, height: 7.5),
+                  Container(
+                      margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              outputBox.addProduct("0360002914523",
+                                  price: 13.37);
+                              money.addMoney(13.37);
+                            },
+                            icon: Image.asset(
+                                "assets/products/Lektira - Voda.jpg"),
+                            iconSize: 250,
+                          ),
+                          const Text("Lektira - Voda (13.37 €)")
+                        ],
+                      )),
+                ],
+              ),
+              const SizedBox(width: 30, height: 30),
+              Column(children: [
+                Container(
+                    margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Column(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            outputBox.addProduct("9789542991152", price: 21.06);
+                            money.addMoney(21.06);
+                          },
+                          icon: Image.asset(
+                              "assets/products/Knjiga - Željko Kovačević - C++, analiza i primjena.jpg"),
+                          iconSize: 250,
+                        ),
+                        const Text(
+                            "Željko Kovačević - C++, analiza i \nprimjena (21.06 €)"),
+                      ],
+                    )),
+                const SizedBox(width: 25, height: 6.5),
+                Container(
+                    margin: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    child: Column(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            outputBox.addProduct("9789151976152", price: 3.32);
+                            money.addMoney(3.32);
+                          },
+                          icon: Image.asset(
+                              "assets/products/Orink magenta - tinta za printer.jpg"),
+                          iconSize: 250,
+                        ),
+                        const Text("Orink magenta - tinta za printer (3.32 €)")
+                      ],
+                    )),
+              ]),
+            ],
+          ))),
     );
   }
 }
